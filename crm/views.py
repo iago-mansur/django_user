fc7d8b5d235e30fe6d1e30f0d19a0049f0188485
iago-mansur <iagoma@gmail.com>
2024-08-01 09:32:32 -0300

    [feat] add: URL's, Views and Templates

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def homepage(request):
+    return render(request, 'crm/index.html')
+
+
+def register(request):
+    return render(request, 'crm/register.html')
+
+
+def my_login(request):
+    return render(request, 'crm/my-login.html')
+
+
+def dashboard(request):
+    return render(request, 'crm/dashboard.html')
